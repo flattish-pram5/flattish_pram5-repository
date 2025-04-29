12c229cf9555ac61271142f60dfeb3f7b0f37dbb
flattish_pram5 <vadim040209@gmail.com>
2025-04-29 10:41:49 +0300

    Add files via upload

---
+++ Site-sign-in-up/Scripts/script-form-sign-in.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const email = document.querySelector('input[type="email"]').value;
+        const passwordInput = document.querySelector('#password-input');
+
+        if (!passwordInput) {
+            console.error('Password input not found');
+            return;
+        }
+
+        const password = passwordInput.value;
+
+        fetch('http://localhost:3000/sign-in', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            console.log(data);
+        });
+
+        this.reset();
+    });
+});
